a7a5c5d1adb07606e55831938745310c200add41
Freja Gudmandsen <ferling.gud@gmail.com>
2022-09-05 13:45:35 +0200

    Studerende+klasse+class

---
+++ ClassRoomNet60/KlasseRum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class KlasseRum
+    {
+		private string klasseNavn;
+
+		public string Klassenavn
+		{
+			get { return klasseNavn; }
+			set { klasseNavn = value; }
+		}
+
+		private List<Studerende> getStuderende;
+
+		public List<Studerende> GetStuderende
+		{
+			get { return GetStuderende; }
+			set { GetStuderende = value; }
+		}
+
+		private DateOnly semesterStart;
+
+		public DateOnly GetSemesterStart
+		{
+			get { return semesterStart; }
+			set { semesterStart = value; }
+		}
+	}
+}
---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Studerende
+    {
+		private String studentName;
+
+		public String StudentName
+		{
+			get { return studentName; }
+			set { studentName = value; }
+		}
+
+		private int myBirthMonth;
+
+		public int MyBirthmonth
+		{
+			get { return myBirthMonth; }
+			set { myBirthMonth = value; }
+		}
+
+
+		private int myBirthday;
+
+		public int MyBirthday
+		{
+			get { return myBirthday; }
+			set { myBirthday = value; }
+		}
+
+		public Studerende()
+		{
+
+		}
+		public Studerende(string studentName, int myBirthMonth, int myBirthday)
+		{
+			StudentName = studentName;
+			//StudentName = studentName;
+			//this.myBirthMonth = myBirthMonth;
+			this.MyBirthmonth = myBirthMonth;
+			MyBirthday = myBirthday;
+			//MyBirthday = myBirthday;
+		}	
+	}
+}
